784adc21d3f0f527475b206841e92253ba922702
Sam A. Horvath-Hunt <samhh.web@gmail.com>
2016-09-21 18:03:41 +0100

    More specific

--- generators/app/sortNpmDeps.js
+++ generators/app/sortNpmDeps.js
@@ -1,5 +1,5 @@
 // Pass in an object (package.json) and this will sort the dependencies alphabetically
-// As objects are passed by reference in JS this won't return anything but will instead mutate the object directly
+// As objects are passed by reference in JS (effectively for our purporses at least... if you're curious, see: http://stackoverflow.com/a/13104500/3369753) this won't return anything but will instead mutate the object directly
 // This is absolutely not needed but a nice-to-have :-)
 
 module.exports = function (deps) {
