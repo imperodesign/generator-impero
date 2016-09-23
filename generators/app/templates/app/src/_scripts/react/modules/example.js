f3ed550575677a20ffe3cca827f28129b4843a53
Sam A. Horvath-Hunt <samhh.web@gmail.com>
2016-09-23 12:15:02 +0100

    Reduce example cruft

--- generators/app/templates/app/src/_scripts/react/modules/example.js
+++
@@ -1,7 +0,0 @@
-export default function (app) {
-  console.log('Hello world!')
-
-  app.on('exampleEmission', () => {
-    console.log('Event emitter working.')
-  })
-}
--- generators/app/templates/app/src/_scripts/vanilla/main.js
+++ generators/app/templates/app/src/_scripts/vanilla/main.js
@@ -26,20 +26,8 @@ import 'classlist.js'
 import 'es6-promise'
 import 'matchmedia-polyfill'
 <% } %>
-// Event emitter to allow communication between modules
-import {EventEmitter} from 'events'
-const app = new EventEmitter()
-
-// And finally our modules for this project
-// These two modules demonstrate the event emitter
-import example from './modules/example'
-example(app)
-
-import example2 from './modules/example2'
-example2(app)
-
 // This code demonstrates the code splitting/chunking ability of Webpack
-// The example3 module will only be loaded over the network upon us triggering the require code below
+// The example module will only be loaded over the network upon us triggering the require code below
 const exampleEl = document.querySelector('.js-example')
 const exampleTriggerEl = document.querySelector('.js-example-trigger')
 
@@ -47,6 +35,6 @@ if (exampleEl && exampleTriggerEl) {
   exampleTriggerEl.addEventListener('submit', event => {
     event.preventDefault()
 
-    require.ensure([], require => require('./modules/example3').default(exampleEl))
+    require.ensure([], require => require('./modules/example-require-ensure').default(exampleEl))
   })
 }
