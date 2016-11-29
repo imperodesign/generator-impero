40cccad96c24f981607786d303fa7fb3300bb587
Sam A. Horvath-Hunt <samhh.web@gmail.com>
2016-11-29 11:22:29 +0000

    Fix another regression

--- generators/app/templates/app/server-routes.js
+++ generators/app/templates/app/server-routes.js
@@ -4,7 +4,9 @@ module.exports = app => {
   <% if (jsLang === 'vue') { %>app.post('/api', (req, res) => {
     // Do something here
   })<% } else { %>// Pass all GET requests onto the client router
-  app.get('*', (req, res) => {
-    res.render('base')
+  app.get('/', (req, res) => {
+    res.render('pages/home', {
+      title: 'Home'
+    })
   })<% } %>
 }
